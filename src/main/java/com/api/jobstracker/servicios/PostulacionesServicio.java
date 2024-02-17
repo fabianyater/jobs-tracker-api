e91b9d132794b1eed06fda4803c8a6c8e37dd188
Fabian Yate <fabianyate31@gmail.com>
2024-02-17 00:36:10 -0500

    add new param for filtering

--- src/main/java/com/api/jobstracker/servicios/PostulacionesServicio.java
+++ src/main/java/com/api/jobstracker/servicios/PostulacionesServicio.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface PostulacionesServicio {
     void agregarPostulacion(PostulacionSolicitud postulacionSolicitud);
-    PostulacionRespuestaPaginada listarPostulaciones(int currentPage, int itemsPerPage);
+    PostulacionRespuestaPaginada listarPostulaciones(int currentPage, int itemsPerPage, List<String> estado);
     void eliminarPostulacion(Integer postulacionId);
     void actualizarEstado(Integer id, EstadoSolicitud estado);
     List<PostulacionTimelineRespuesta> obtenerPostulacionesTimeline(Integer postulacionId);
