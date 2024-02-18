1105f3f0777acd6c1f1eae1bd5ca2ad31acc6043
Fabian Yate <fabianyate31@gmail.com>
2024-02-17 19:07:41 -0500

    add new property to set the name of the platform where the application belongs

--- src/main/java/com/api/jobstracker/dominio/modelo/Postulacion.java
+++ src/main/java/com/api/jobstracker/dominio/modelo/Postulacion.java
@@ -25,6 +25,9 @@ public class Postulacion {
     @Column(name = "descpription", columnDefinition = "text")
     private String descripcion;
 
+    @Column(name = "plaftorm_name")
+    private String platformName;
+
     @Column(name = "current_status")
     private String currentStatus;
 
